561b8197f635d27bcd1c4b5e2feb3a8302090dcb
1Ruslan3 <keshabyn@mail.ru>
2025-07-17 21:17:55 +0300

    create service

---
+++ internal/service/service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"subscription-service/internal/model"
+	"subscription-service/internal/repository"
+	"time"
+)
+
+type SubscriptionService struct {
+	repo *repository.SubscriptionRepository
+}
+
+func NewSubscriptionService(r *repository.SubscriptionRepository) *SubscriptionService {
+	return &SubscriptionService{repo: r}
+}
+
+func (s *SubscriptionService) Create(sub *model.Subscription) error {
+	return s.repo.Create(sub)
+}
+
+func (s *SubscriptionService) GetAll() ([]model.Subscription, error) {
+	return s.repo.GetAll()
+}
+
+func (s *SubscriptionService) GetByID(id uint) (*model.Subscription, error) {
+	return s.repo.GetByID(id)
+}
+
+func (s *SubscriptionService) Update(sub *model.Subscription) error {
+	return s.repo.Update(sub)
+}
+
+func (s *SubscriptionService) Delete(id uint) error {
+	return s.repo.Delete(id)
+}
+
+func (s *SubscriptionService) CalculateTotal(from, to time.Time, userID, serviceName string) (int, error) {
+	return s.repo.CalculateTotal(from, to, userID, serviceName)
+}
